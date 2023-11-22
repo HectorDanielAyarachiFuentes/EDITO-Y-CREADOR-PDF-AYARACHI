f8ba69770009c8d0db2b59996c5df8dbc2368705
HectorDanielAyarachiFuentes <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-11-22 15:07:33 -0300

    123456

--- script.js
+++ script.js
@@ -10,12 +10,9 @@ tinymce.init({
           'alignleft aligncenter alignright alignjustify | ' +
           'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help',
       });
-
 //*Script guardar el PDF/
-
       async function saveAsPDF() {
   const content = tinymce.get('mytextarea').getContent();
-
   const options = {
     margin: 10,
     filename: 'mi_archivo.pdf',
@@ -23,9 +20,7 @@ tinymce.init({
     html2canvas: { scale: 2 },
     jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
   };
-
   const pdfBlob = await html2pdf().from(content).set(options).toPdf().output('blob');
-
   const link = document.createElement('a');
   link.href = URL.createObjectURL(pdfBlob);
   link.download = 'mi_archivo.pdf';
